44b2a745ac58efad5e7ae309ad3229cde9a13d4d
Nilalochan BV <nilalochanbv@gmail.com>
2026-07-09 09:50:31 +0530

    Create 21-MergeTwoSortedLists

---
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
@@ -0,0 +1,36 @@
+// Last updated: 7/9/2026, 9:50:30 AM
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) return list1;
+        else if (list1 == null || list2 == null) return list1 == null ? list2 : list1;
+        ListNode d = null;
+        ListNode td = null;
+        while (list1 != null && list2 != null){
+            if (list1.val <= list2.val) {
+                if (d == null) {
+                    d = list1;
+                    td = list1;
+                }
+                else {
+                    d.next = list1;
+                    d = list1;
+                }
+                list1 = list1.next;
+            }
+            else {
+                if (d == null) {
+                    d = list2;
+                    td = list2;
+                }
+                else {
+                    d.next = list2;
+                    d = list2;
+                }
+                list2 = list2.next;
+            }
+        }
+        if (list1 != null) d.next = list1;
+        else if (list2 != null) d.next = list2;
+        return td; 
+    }
+}
