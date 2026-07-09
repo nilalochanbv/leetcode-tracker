bcadfa97554d6a728e771b5a952d17548bebe61c
Nilalochan BV <nilalochanbv@gmail.com>
2026-07-09 09:48:19 +0530

    Create 495-TeemoAttacking

---
+++ 495-TeemoAttacking/495-TeemoAttacking.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 9:48:18 AM
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int total = 0;
+        for (int i = 0; i < timeSeries.length-1; i++) {
+            // if next attack occurs before current duration ends, include the difference
+            if (timeSeries[i+1] <= timeSeries[i] + duration-1) {
+                total += timeSeries[i+1] - timeSeries[i];
+            } else { // add duration normally
+                total += duration;
+            }
+        }
+        total += duration; // include last attack from teemo
+        return total;
+    }
+}
