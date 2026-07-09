497330334363694c329cd8f05dc269ce29b5140d
Nilalochan BV <nilalochanbv@gmail.com>
2026-07-09 09:49:50 +0530

    Create 101-SymmetricTree

---
+++ 101-SymmetricTree/101-SymmetricTree.py
@@ -0,0 +1,14 @@
+# Last updated: 7/9/2026, 9:49:48 AM
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        
+        def is_mirror(n1, n2): # n1:left, n2:right
+            if not n1 and not n2:
+                return True
+            
+            if not n1 or not n2:
+                return False
+            
+            return n1.val == n2.val and is_mirror(n1.left, n2.right) and is_mirror(n1.right, n2.left)
+        
+        return is_mirror(root.left, root.right)
