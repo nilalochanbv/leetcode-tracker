2c06ff382854ef56acde1f30dfb37feb2000d8a4
Nilalochan BV <nilalochanbv@gmail.com>
2026-07-09 09:48:47 +0530

    Create 326-PowerOfThree

---
+++ 326-PowerOfThree/326-PowerOfThree.java
@@ -0,0 +1,8 @@
+// Last updated: 7/9/2026, 9:48:45 AM
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n <= 0) return false;
+        while(n%3==0) n/=3;
+        return n==1;
+    }
+}
