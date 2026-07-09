87b0ad5df3e85748903c2de1a7543d73e5fcd266
Nilalochan BV <nilalochanbv@gmail.com>
2026-07-09 09:46:45 +0530

    Create 2236-MaximumTwinSumOfALinkedList

---
+++ 2236-MaximumTwinSumOfALinkedList/2236-MaximumTwinSumOfALinkedList.java
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 9:46:43 AM
+class Solution {
+    public int pairSum(ListNode head) {
+        Stack<Integer> half = new Stack<>();
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            half.push(slow.val);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        int res = 0;
+        while (slow != null) {
+            res = Math.max(res, half.pop() + slow.val);
+            slow = slow.next;
+        }
+
+        return res;
+    }
+}
