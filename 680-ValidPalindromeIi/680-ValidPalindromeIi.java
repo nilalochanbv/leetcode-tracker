9ed99310b297245671807c4ddf166a906df97e08
Nilalochan BV <nilalochanbv@gmail.com>
2026-07-09 09:48:05 +0530

    Create 680-ValidPalindromeIi

---
+++ 680-ValidPalindromeIi/680-ValidPalindromeIi.java
@@ -0,0 +1,25 @@
+// Last updated: 7/9/2026, 9:48:04 AM
+class Solution {
+    public boolean isPalindrome(String s,int left,int right){
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        int left=0;
+        int right=s.length()-1;
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return isPalindrome(s,left+1,right) || isPalindrome(s,left,right-1);
+                }
+                left++;
+                right--;
+            }
+    return true;
+    }
+}
