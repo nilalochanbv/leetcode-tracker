7faaa4919ef1c68a8f95bda0b0fe424b455185a3
Nilalochan BV <nilalochanbv@gmail.com>
2026-07-09 09:49:51 +0530

    Create 100-SameTree

---
+++ 100-SameTree/100-SameTree.py
@@ -0,0 +1,10 @@
+# Last updated: 7/9/2026, 9:49:50 AM
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        return False
