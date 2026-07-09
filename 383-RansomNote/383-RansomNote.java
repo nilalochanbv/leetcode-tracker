de02521a2e354e018110ea222dc7102dd85ea05d
Nilalochan BV <nilalochanbv@gmail.com>
2026-07-09 09:48:36 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 9:48:34 AM
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+		if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabets_counter = new int[26];
+        
+        for (char c : magazine.toCharArray())
+            alphabets_counter[c-'a']++;
+
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) return false;
+            alphabets_counter[c-'a']--;
+        }
+        return true;
+    }
+}
