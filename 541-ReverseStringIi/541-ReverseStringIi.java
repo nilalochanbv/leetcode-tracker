bee6afd83460ace1ab4e49d8b7fcc5c6c9adc1bd
Nilalochan BV <nilalochanbv@gmail.com>
2026-07-09 09:48:14 +0530

    Create 541-ReverseStringIi

---
+++ 541-ReverseStringIi/541-ReverseStringIi.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 9:48:12 AM
+class Solution {
+    public String reverseStr(String s, int k) {
+        char arr[] =s.toCharArray();
+        int n =arr.length;
+        for(int i=0;i<n;i+=2*k){
+            int left =i;
+            int right=Math.min(left + k-1 , n-1);
+        while(left < right){
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        } 
+    }
+        return new String(arr);
+    }
+}
