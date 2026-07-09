028dee44bd0c46e1969b1b26f276f052f2146686
Nilalochan BV <nilalochanbv@gmail.com>
2026-07-09 09:46:42 +0530

    Create 2265-PartitionArrayAccordingToGivenPivot

---
+++ 2265-PartitionArrayAccordingToGivenPivot/2265-PartitionArrayAccordingToGivenPivot.java
@@ -0,0 +1,33 @@
+// Last updated: 7/9/2026, 9:46:40 AM
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        int left = 0;
+        int right = n - 1;
+
+        int i = 0;
+        int j = n - 1;
+
+        while (i < n) {
+            if (nums[i] < pivot) {
+                ans[left++] = nums[i];
+            }
+
+            if (nums[j] > pivot) {
+                ans[right--] = nums[j];
+            }
+
+            i++;
+            j--;
+        }
+
+        // Remaining positions contain pivot
+        while (left <= right) {
+            ans[left++] = pivot;
+        }
+
+        return ans;
+    }
+}
