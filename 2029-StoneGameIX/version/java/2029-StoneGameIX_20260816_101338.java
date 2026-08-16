b9c5c0195dc085edc4351ef84f93191916cceb11
Nilalochan BV <nilalochanbv@gmail.com>
2026-08-16 10:13:36 +0530

    Create 2029-StoneGameIX

---
+++ 2029-StoneGameIX/version/java/2029-StoneGameIX_20260816_101338.java
@@ -0,0 +1,13 @@
+// Last updated: 8/16/2026, 10:13:38 AM
+1class Solution {
+2    public boolean stoneGameIX(int[] stones) {
+3        int []arr = new int[3];
+4        for(int s : stones){
+5            arr[s%3]++;
+6        }
+7        if(arr[0]%2==0){
+8            return arr[1]>=1 && arr[2]>=1;
+9        }
+10        return Math.abs(arr[1]-arr[2])>2;
+11    }
+12}
