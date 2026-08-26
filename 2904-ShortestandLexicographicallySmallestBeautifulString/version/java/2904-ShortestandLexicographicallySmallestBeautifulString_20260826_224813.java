aa89a4e4763706b148b9a4870196c4f1c6db014d
Nilalochan BV <nilalochanbv@gmail.com>
2026-08-26 22:48:17 +0530

    Create 2904-ShortestandLexicographicallySmallestBeautifulString

---
+++ 2904-ShortestandLexicographicallySmallestBeautifulString/version/java/2904-ShortestandLexicographicallySmallestBeautifulString_20260826_224813.java
@@ -0,0 +1,38 @@
+// Last updated: 8/26/2026, 10:48:13 PM
+1class Solution {
+2    public String shortestBeautifulSubstring(String s, int k) {
+3        String ans = "";
+4        int n = s.length();
+5
+6        for (int i = 0; i < n; i++) {
+7
+8            int oneCnt = 0;
+9            StringBuilder cur = new StringBuilder();
+10
+11            for (int j = i; j < n; j++) {
+12
+13                cur.append(s.charAt(j));
+14
+15                if (s.charAt(j) == '1')
+16                    oneCnt++;
+17
+18                // More than k ones can never become valid again
+19                if (oneCnt > k)
+20                    break;
+21
+22                if (oneCnt == k) {
+23                    String curStr = cur.toString();
+24
+25                    if (ans.isEmpty() ||
+26                        curStr.length() < ans.length() ||
+27                        (curStr.length() == ans.length() && curStr.compareTo(ans) < 0)) {
+28
+29                        ans = curStr;
+30                    }
+31                }
+32            }
+33        }
+34
+35        return ans;
+36    }
+37}
