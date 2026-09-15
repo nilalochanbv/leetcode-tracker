8c506d5b1bd8ce99afb69646490dd3da5c61bdef
Nilalochan BV <nilalochanbv@gmail.com>
2026-09-15 12:33:16 +0530

    Time: 14 ms (91.74%), Space: 43.1 MB (97.54%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -4,16 +4,21 @@ class Solution {
         return s;
         int start=0;
         int end=0;
+        int max=0;
         for(int i=0;i<s.length();i++){
             int len1 = expand(s,i,i);
             int len2 = expand (s,i,i+1);
             int len =Math.max(len1,len2);
-             if(len > end-start){
+            //  if(len > end-start){
+            //     start = i - (len-1)/2;
+            //     end= i+len/2;
+            //  }
+            if(len>max){
+                max=len;
                 start = i - (len-1)/2;
-                end= i+len/2;
-             }
+            }
         }
-             return s.substring(start,end+1);
+            return s.substring(start,start+max);
     }
     public int expand(String s, int left,int right){
         while(left>=0 && right<s.length() && s.charAt(left)== s.charAt(right)){
